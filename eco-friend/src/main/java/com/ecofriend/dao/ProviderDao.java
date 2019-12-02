abc60176fa11dd277d4344322494da42f34c8643
JohnFromMars <rock13810@gmail.com>
2019-12-02 12:00:56 +0800

    Dao comment

--- eco-friend/src/main/java/com/ecofriend/dao/ProviderDao.java
+++ eco-friend/src/main/java/com/ecofriend/dao/ProviderDao.java
@@ -6,9 +6,21 @@ import org.springframework.stereotype.Repository;
 import com.ecofriend.model.Provider;
 import com.ecofriend.model.SiteUser;
 
+/**
+ * Provider Dao
+ * 
+ * @author user
+ *
+ */
 @Repository
 public interface ProviderDao extends CrudRepository<Provider, Long> {
 
+	/**
+	 * return the provider object based on the given site user
+	 * 
+	 * @param siteUser
+	 * @return
+	 */
 	Provider findBySiteUser(SiteUser siteUser);
 
 }
--- eco-friend/src/main/java/com/ecofriend/dao/SenderDao.java
+++ eco-friend/src/main/java/com/ecofriend/dao/SenderDao.java
@@ -4,9 +4,23 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.ecofriend.model.Sender;
 import com.ecofriend.model.SiteUser;
-
+/**
+ * Sender Dao
+ * @author user
+ *
+ */
 public interface SenderDao extends CrudRepository<Sender, Long> {
+	/**
+	 * return a sender object based on the given site user information
+	 * @param user
+	 * @return
+	 */
 	Sender findByUser(SiteUser user);
 
+	/**
+	 * return the sender object based on the given license number
+	 * @param licenseNo
+	 * @return
+	 */
 	Sender findByLicensNumber(String licenseNo);
 }
